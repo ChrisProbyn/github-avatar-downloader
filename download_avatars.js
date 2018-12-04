3807928013a74ed76d2bb65c4522a47bf4c141e3
ChrisProbyn <cprobyn16@gmail.com>
2018-12-04 00:18:21 +0000

    returns avatars url

--- download_avatars.js
+++ download_avatars.js
@@ -1,5 +1,40 @@
 var request = require('request');
 var fs = require('fs');
+var secret = require('./secrets');
+
+function getRepoContributors(repoOwner, repoName, callback) {
+  var options = {
+    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    headers: {
+      'User-Agent': 'request',
+      'Authorization': 'token ' + secret.GITHUB_TOKEN
+    },
+  };
+
+// request.get(options.url)
+//        .on('response', function (response) {
+//         var data = JSON.parse(response).headers["avatar_url"]
+
+//        });
 
 
-console.log('Welcome to the GitHub Avatar Downloader!');
+  request(options, function(err, res, body) {
+
+
+    var resulting = JSON.parse(body);
+    var list = [];
+    for(var i of resulting){
+     list.push(i.avatar_url);
+    }
+
+
+
+    callback(err,list);
+  });
+}
+
+
+getRepoContributors("jquery", "jquery", function(err, result) {
+  console.log("Errors:", err);
+  console.log("Result:", result);
+});
